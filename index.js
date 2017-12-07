290044f4e777bab14030e0dda3dba848918218fe
Vitaly Nikitin <vit.nikitin@gmail.com>
2017-12-07 12:23:47 +0300

    add maching by city using regexp

--- index.js
+++ index.js
@@ -1,48 +1,51 @@
-var countryMapper = require('country-mapper');
-var mapperData = require('./content/mapper.json');
-var country = require('country-data');
-var lookup = country.lookup;
+const countryMapper = require('country-mapper')
+const mapperData = require('./content/mapper.json')
+const country = require('country-data')
+const lookup = country.lookup
 
-var getAreaCode = function (country, city) {
-    var data = _findData(country, city) || {};
-    return (data.area_code || null);
+let getAreaCode = (country, city) => {
+  let data = _findData(country, city) || {}
+  return (data.area_code || null)
+}
+
+let getDialingCode = (country, city) => {
+  let data = _findData(country, city) || {}
+  let callingCode = data.dialing_code
+  if (callingCode) {
+    return (data.dialing_code)
+  }
+  let alpha2 = countryMapper.convert(country)
+  let countries = lookup.countries({
+    alpha2
+  })
+  let ref = countries[0]
+  return (ref != null ? ref.countryCallingCodes[0] : null)
 };
 
-var getDialingCode = function (country, city) {
-    var data = _findData(country, city) || {};
-    var callingCode = data.dialing_code;
-    if (callingCode) {
-        return (data.dialing_code);
+let _findData = (country, city) => {
+  if (!city || !country) return (null)
+  city = city.toLowerCase()
+  let alpha2 = countryMapper.convert(country) || country
+  let paths = mapperData[alpha2] || []
+  for (let i = 0; i < paths.length; ++i) {
+    let cities = require('./content' + paths[i])
+    let data = cities.find((item) => {
+      let name = item.city || ''
+      // stateCapital and state contains in USA regions
+      let stateCapital = item.state_capital || ''
+      let state = item.state || ''
+      return name && new RegExp(name, 'i').test(city) ||
+        stateCapital && new RegExp(stateCapital, 'i').test(city) ||
+        state && new RegExp(state, 'i').test(city)
+    })
+    if (data) {
+      return (data)
     }
-    var alpha2 = countryMapper.convert(country);
-    var countries = lookup.countries({
-        alpha2: alpha2
-    });
-    var ref;
-    return ((ref = countries[0]) != null ? ref.countryCallingCodes[0] : void 0);
-};
-
-var _findData = function (country, city) {
-    if (!city || !country) return (null);
-    city = city.toLowerCase();
-    var alpha2 = countryMapper.convert(country) || country;
-    var paths = mapperData[alpha2] || [];
-    for (var i = 0; i < paths.length; ++i) {
-        var cities = require('./content' + paths[i]);
-        var data = cities.find(function (item) {
-            var name = item.city || '';
-            var stateCapital = item.state_capital || '';
-            var state = item.state || '';
-            return city == name.toLowerCase() || city == stateCapital.toLowerCase() || city == state.toLowerCase();
-        });
-        if (data) {
-            return (data);
-        }
-    }
-    return (null);
-};
+  }
+  return (null)
+}
 
 module.exports = {
-    getAreaCode: getAreaCode,
-    getDialingCode: getDialingCode
-};
+  getAreaCode,
+  getDialingCode
+}
--- test/index.js
+++ test/index.js
@@ -1,35 +1,35 @@
-var chai = require('chai');
-var assert = chai.assert;
-var dialcodes = require('../index');
+const chai = require('chai')
+const assert = chai.assert
+const dialcodes = require('../index')
 
-describe('dialcodes', function () {
+describe('dialcodes', () => {
 
-    describe('get area code', function () {
-        it('should return area code by Country and City name', function () {
-            assert.equal(495, dialcodes.getAreaCode('Russia', 'Moscow'));
-            assert.equal(916, dialcodes.getAreaCode('United States', 'California'));
-            assert.equal(805, dialcodes.getAreaCode('United States', 'santa barbara'));
-            assert.equal(415, dialcodes.getAreaCode('United States', 'san francisco'));
-            assert.equal(17, dialcodes.getAreaCode('Belarus', 'Minsk'));
-            assert.equal(812, dialcodes.getAreaCode('Russia', 'St. Petersburg'));
-            assert.equal(null, dialcodes.getAreaCode('Unknown'));
-            assert.equal(null, dialcodes.getAreaCode());
-        });
+  describe('get area code', () => {
+    it('should return area code by Country and City name', () => {
+      assert.equal(495, dialcodes.getAreaCode('Russia', 'Moscow'))
+      assert.equal(916, dialcodes.getAreaCode('United States', 'California'))
+      assert.equal(805, dialcodes.getAreaCode('United States', 'santa barbara'))
+      assert.equal(415, dialcodes.getAreaCode('United States', 'san francisco'))
+      assert.equal(17, dialcodes.getAreaCode('Belarus', 'Minsk'))
+      assert.equal(812, dialcodes.getAreaCode('Russia', 'St. Petersburg'))
+      assert.equal(null, dialcodes.getAreaCode('Unknown'))
+      assert.equal(null, dialcodes.getAreaCode())
     });
+  });
 
-    describe('get dialing code', function () {
-        it('should return dialing code by Country and City name', function () {
-            assert.equal('+7 495', dialcodes.getDialingCode('Russia', 'Moscow'));
-            assert.equal('+1 916', dialcodes.getDialingCode('United States', 'California'));
-            assert.equal('+1 805', dialcodes.getDialingCode('United States', 'santa barbara'));
-            assert.equal('+1 415', dialcodes.getDialingCode('United States', 'san francisco'));
-            assert.equal('+375 17', dialcodes.getDialingCode('Belarus', 'Minsk'));
-            assert.equal('+7 812', dialcodes.getDialingCode('Russia', 'St. Petersburg'));
-            assert.equal('+7', dialcodes.getDialingCode('Russia'));
-            assert.equal('+7', dialcodes.getDialingCode('Russia', 'Unknown city'));
-            assert.equal(null, dialcodes.getDialingCode('Unknown'));
-            assert.equal(null, dialcodes.getDialingCode());
-        });
-    });
+  describe('get dialing code', () => {
+    it('should return dialing code by Country and City name', () => {
+      assert.equal('+7 495', dialcodes.getDialingCode('Russia', 'Moscow'))
+      assert.equal('+1 916', dialcodes.getDialingCode('United States', 'California'))
+      assert.equal('+1 805', dialcodes.getDialingCode('United States', 'santa barbara'))
+      assert.equal('+1 415', dialcodes.getDialingCode('United States', 'san francisco'))
+      assert.equal('+375 17', dialcodes.getDialingCode('Belarus', 'Minsk'))
+      assert.equal('+7 812', dialcodes.getDialingCode('Russia', 'St. Petersburg'))
+      assert.equal('+7', dialcodes.getDialingCode('Russia'))
+      assert.equal('+7', dialcodes.getDialingCode('Russia', 'Unknown city'))
+      assert.equal(null, dialcodes.getDialingCode('Unknown'))
+      assert.equal(null, dialcodes.getDialingCode())
+    })
+  })
 
-});
+})
